c9a2fc9174aaa8c9b4aef4573101d528d8e7a4fa
Boris van Hoytema <i@bvh.me>
2015-05-21 09:29:34 +0000

    API

--- server.js
+++ server.js
@@ -2,14 +2,21 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-  res.send('Het NCTB Terreurniveau is: "Substantieel"');
+    res.set('Content-Type', 'text/html');
+    res.send('Het NCTB Terreurniveau is: "Substantieel"');
 });
+
+app.get('/api', function (req, res) {
+    res.set('Content-Type', 'application/json');
+    res.send({ "NCTB DTN": "Substantieel" });
+});
+
 
 var server = app.listen(8080, function () {
 
   var host = server.address().address;
   var port = server.address().port;
 
-  console.log('Example app listening at http://%s:%s', host, port);
+  console.log('The fear index is running on port %s', host, port);
 
 });
