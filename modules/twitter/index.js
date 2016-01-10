b48a2b522c6196887464f5ea4e80e40a3ddaf71f
Wouter Vroege <woutervroege@MacbookProWV.home>
2016-01-10 13:30:47 +0100

    refactoring

---
+++ modules/twitter/index.js
@@ -0,0 +1,53 @@
+/*
+ * @package charli
+ * @subpackage twitter
+ * @copyright Copyright(c) 2016 Paranoia Watch
+ * @author Boris van Hoytema <boris AT newatoms DOT com>
+ */
+
+require('dotenv').config({silent: true})
+var tracker = require('./lib/tracker')
+var parseTweet = require('./lib/tweet-parser')
+var settings = require('../../settings.js').twitterTracker
+var events = require('events')
+
+function trackStream(termsArray) {
+  if(settings.disabled)
+    return shutdown('Twitter tracking is disabled in the settings')
+
+  var streamTracker = new tracker({
+      terms: termsArray,
+      consumerKey: settings.consumerKey,
+      consumerSecret: settings.consumerSecret,
+      accessToken: settings.accessToken,
+      accessSecret: settings.accessSecret
+  })
+  return streamTracker
+}
+
+function getPublicationStream(searchTerms) {
+    var publicationStream = new events.EventEmitter()
+    var twitterTrack = trackStream(searchTerms)
+
+    twitterTrack.on('connected', function() {
+      publicationStream.emit('connected')
+    })
+
+    twitterTrack.on('connection-error', function(error) {
+      publicationStream.emit('connection-error', error)
+    })
+
+    twitterTrack.on('tweet', function(tweet) {
+      var publication = parseTweet(tweet)
+      publicationStream.emit('publication', publication)
+    })
+
+    return publicationStream
+}
+
+function shutdown(message) {
+  console.log(message);
+  process.exit()
+}
+
+exports.publisher = getPublicationStream
---
+++ modules/twitter/lib/tweet-parser.js
@@ -0,0 +1,15 @@
+/*
+ * @package charli
+ * @subpackage twitter
+ * @copyright Copyright(c) 2016 Paranoia Watch
+ * @author Boris van Hoytema <boris AT newatoms DOT com>
+ * @author Wouter Vroeee <wouter AT woutervroege DOT nl>
+ */
+
+var TweetParser = function (tweet) {
+    return {
+        id: tweet.id
+    }
+}
+
+module.exports = TweetParser
