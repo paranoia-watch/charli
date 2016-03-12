b0a7ef4d5617ce7b08b3759f4f5c900f3a678825
bvhme <i@bvh.me>
2016-03-12 11:07:32 +0000

    get current average for previous publication

--- modules/update-average/index.js
+++ modules/update-average/index.js
@@ -7,13 +7,20 @@ mongoose.connect('mongodb://paranoia:977KvtG^DXray.W^xaT97a3oFJiVYEJwA@db.parano
 function connectionCallback(err) {
   if(err) return console.error(err);
   getSinglePublication(function(err, data) {
-    console.info(err, data)
   })
 }
 
 function getSinglePublication(callback) {
-  PublicationModel.find({collectionAverageAfterInsert: {$ne: true}}).sort({date: 1}).limit(1).exec(function(err, records) {
+  PublicationModel.find({collectionAverageAfterInsert: {$exists: false}}).sort({date: 1}).limit(1).exec(function(err, records) {
     if(err || !records || !records[0]) return callback(err || "no records found")
     callback(null, records[0])
+  })
+}
+
+function getCurrentAverageForPreviousPublication(callback) {
+  PublicationModel.find({collectionAverageAfterInsert: {$exists: true}}).sort({date: -1}).limit(1).exec(function(err, records) {
+    if(err) return callback(err)
+    if(!records || !records[0]) return callback(null, 0)
+    callback(null, records[0].collectionAverageAfterInsert)
   })
 }
